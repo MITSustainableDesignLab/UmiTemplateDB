85c4d1f244ceb00081539074923d8ed9dbf0abca
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-08-22 10:01:51 -0400

    setup.py installs requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import os
+
+import path
 from setuptools import setup
+
+here = os.getcwd()
+
+with open(path.join(here, "requirements.txt")) as f:
+    requirements_lines = f.readlines()
+install_requires = [r.strip() for r in requirements_lines]
 
 setup(
     name="umitemplatedb",
@@ -6,6 +15,7 @@ setup(
     packages=["tests", "umitemplatedb"],
     url="",
     license="",
+    install_requires=install_requires,
     author="Samuel Letellier-Duchesne",
     author_email="samueld@mit.edu",
     description="Functions to import and query the UmiTemplate DataBase",
