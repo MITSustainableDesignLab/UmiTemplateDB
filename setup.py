370811bf264c0439fb7541fd3124412642a91b5a
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-05-24 18:58:17 -0400

    3.7 3.8

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
 )
